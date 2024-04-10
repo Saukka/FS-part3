a1a3913e2ccbacf3aa07586edaef0f39f2d6809d
Saukka <sauli.sorvari@kolumbus.fi>
2024-04-10 15:39:23 +0300

    3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('content-body', function (req, res) {
+    if (req.method === 'POST') {
+      return JSON.stringify(req.body)
+    }
+    return })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content-body'))
+
 
 let persons = [
   { id: 1, name: 'Arto Hellas', number: '040-123456' },
