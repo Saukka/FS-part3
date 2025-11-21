0f2f89742192ad42825c124f28d74fbb4478a080
Saukka <sauli.sorvari@kolumbus.fi>
2025-11-21 16:01:50 +0200

    3.19-3.21

--- models/person.js
+++ models/person.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+        validator: function(number) {
+          return /^\d{2,3}-\d{5,15}$/.test(number);
+        },
+        message: props => `${props.value} is not a valid phone number`
+      },
+    required: true
+    } 
 })
 
 personSchema.set('toJSON', {
